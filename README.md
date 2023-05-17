807aa8fe593f88ffc3c3d47c8b6f63c1c5ef5d59
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-05-17 23:29:09 +0200

    Add devenv

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# dotfiles
-My dotfiles
+<!-- markdownlint-disable first-line-heading -->
+
+![header.png](https://raw.githubusercontent.com/shikanime/shikanime/main/assets/github-header.png)
+
+<!-- markdownlint-enable first-line-heading -->
+
+## Documentations
+
+Hey 🌸 I'm Shikanime Deva, this is my dotfiles.
---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Shikanime's dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  nixConfig = {
+    extra-public-keys = [
+      "shikanime.cachix.org-1:OrpjVTH6RzYf2R97IqcTWdLRejF6+XbpFNNZJxKG8Ts="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+    ];
+    extra-substituters = [
+      "https://shikanime.cachix.org"
+      "https://devenv.cachix.org"
+    ];
+  };
+
+  outputs = { nixpkgs, devenv, ... }@inputs: {
+    devShells = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system:
+      let pkgs = import nixpkgs { inherit system; }; in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              pre-commit.hooks = {
+                actionlint.enable = true;
+                markdownlint.enable = true;
+                nixpkgs-fmt.enable = true;
+                statix.enable = true;
+                deadnix.enable = true;
+                prettier.enable = true;
+              };
+              packages = [
+                pkgs.nixpkgs-fmt
+                pkgs.gh
+                pkgs.chezmoi
+              ];
+            }
+          ];
+        };
+      }
+    );
+  };
+}
